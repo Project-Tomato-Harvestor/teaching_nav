765ec28482821f4fe86dfaab2a9f4a6380b80282
Justin10612 <atom.9031@gmail.com>
2024-10-29 14:20:58 +0800

    Change file name

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'teaching_nav'
 
@@ -10,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/learning_mode_launch.py']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
